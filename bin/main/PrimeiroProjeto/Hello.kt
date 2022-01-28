eb05a292ad5317b916cf5d7e673d68699d9abf90
javarodrigues <34476682+JavaRodrigues@users.noreply.github.com>
2022-01-28 17:42:42 -0300

    Primeiros Passos em Kotlin
    Primeiros Passos em Kotlin

--- bin/main/PrimeiroProjeto/Hello.kt
+++ bin/main/PrimeiroProjeto/Hello.kt
@@ -23,8 +23,7 @@ fun main() {
     println("-----")
     println("first: ${lazyMap.first()}")
     println("-----")
-    println("all: ${lazyMap.toList()}"
-    )
+    println("all: ${lazyMap.toList()}")
 
 
     val lazyMap2 = decorations.asSequence().filter { it[0] == 'p' }.map {
